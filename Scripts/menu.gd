4d14e61c828a0dca2907d32a0a6927f2a139b5b3
maloschikpeter <maloschikpeter@gmail.com>
2024-11-07 13:00:42 +0100

    GlobalAudio

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -7,6 +7,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MarginContainer/VBoxContainer/Start.grab_focus()
+	AudioStreamPlayerGlobal.play_music_level()
+	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
